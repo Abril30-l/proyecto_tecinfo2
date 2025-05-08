a5d7e2688017f8cd1b74c086dd2b7adfe6f7084a
Abril30-l <a100243558@my.uvm.edu.mx>
2025-05-08 07:35:13 -0600

    carga de archivos

---
+++ mi_script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const botonSaludo = document.getElementById('botonSaludo');
+
+    botonSaludo.addEventListener('click', function() {
+        alert('¡Hola! Gracias por visitar nuestro proyecto de preservación de anfibios.');
+    });
+});

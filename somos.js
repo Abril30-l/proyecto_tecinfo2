de7d283cbef56ad54b233ab692f2f23f244e7200
Abril30-l <a100243558@my.uvm.edu.mx>
2025-06-05 07:40:40 -0600

    somos.js

---
+++ somos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('El archivo somos.html ha cargado correctamente.');
+
+    // Aquí puedes añadir más interactividad si lo deseas en el futuro
+    // Por ejemplo, animaciones, efectos al hacer scroll, etc.
+});
